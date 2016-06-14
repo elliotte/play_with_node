6812c67e2e9eefa04ecfe9210cdc1b6e5bd7b129
--get <m_elliott@live.com>
2016-06-14 17:43:20 +0100

    tweaks to seo

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Friends of Sullivan | Homepage',
+  	description: 'We are a community of past alumni, friends, family, local parents and more, working together to sustain a future for Sullivan equivalent to the 140 years that have come before us.', 
+  });
 });
 
 /* GET ABOUT page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
+  res.render('about', { 
+  	title: 'Friends of Sullivan | About Us',
+  	description: 'Find out more about Friends of Sullivan, our ethos and how we are building a sustained future for Sullivan.', 
+  });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Express' });
+  res.render('projects', { 
+  	title: 'Friends of Sullivan | Projects',
+  	description: 'Find out more about our latest enhancement projects, and similar projects that have preceded our programme.', 
+  });
 });
 
 /* GET events page. */
 router.get('/events_news', function(req, res, next) {
-  res.render('events', { title: 'Express' });
+  res.render('events', { 
+  	title: 'Friends of Sullivan | Events and News',
+  	description: 'See events being held through out the UK and news updates on Sullivan developments.'
+  });
 });
 
 module.exports = router;
